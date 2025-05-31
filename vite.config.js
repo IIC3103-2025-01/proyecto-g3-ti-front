aa0a83281ef8b54d94e228b6a8a3e6fd1c48b873
anomvlito <84647703+anomvlito@users.noreply.github.com>
2025-05-30 21:23:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,41 +9,22 @@ export default defineConfig(({ command }) => ({
       ? {
           host: "0.0.0.0",
           port: 5173,
-          // Configuración HMR simplificada para desarrollo local
-          hmr: true,
+          hmr: {
+            protocol: "wss",
+            host: "starship3.ing.uc.cl",
+            port: 443,
+            clientPort: 443,
+          },
           proxy: {
             "/api": {
-              target: "https://starship3.ing.uc.cl",
+              target: "http://localhost:8000/",
               changeOrigin: true,
-              secure: true,
+              secure: false,
             },
           },
         }
       : {
-          // al hacer `vite build` no habrá dev-server ni HMR
+          // al hacer vite build no habrá dev-server ni HMR
           hmr: false,
         },
 }));
-
-// para probar la api a nivel local, usar el siguiente código:
-// si es necesario cambiar el puerteo, cambiar el 8000 por el que se va a utilizar:
-// vite.config.js
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       // apunta todas las rutas /api al backend FastAPI
-//       "/api": {
-//         target: "http://localhost:8000",
-//         changeOrigin: true,
-//         secure: false,
-//         // opcional: reescribe la ruta si tu backend no usa el prefijo /api
-//         // rewrite: (path) => path.replace(/^\/api/, "")
-//       },
-//     },
-//   },
-// });
